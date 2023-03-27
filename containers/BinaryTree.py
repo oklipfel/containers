162a45d5419175148303a83c5ab458f83652e014
oklipfel <oklipfe@pitzer.edu>
2023-03-27 15:34:57 -0700

    fixed linter

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -89,7 +89,7 @@ class BinaryTree():
             return self.postorder_print(self.root, '')
         else:
             raise ValueError('Traversal type ' +
- str(traversal_type) + ' is not supported.')
+                             str(traversal_type) + ' is not supported.')
 
     def preorder_print(self, start, traversal):
         '''
@@ -149,8 +149,8 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder(self.root, [])
         else:
-            raise ValueError('Traversal type ' + str(traversal_type) +
- ' is not supported')
+            raise ValueError('Traversal type ' +
+                             str(traversal_type) + ' is not supported')
 
     def preorder(self, start, traversal):
         '''
